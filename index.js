0faf5b83f33029bc977c989e5b130d72d63435cd
Ruth <rwangare124@gmail.com>
2023-04-11 14:59:36 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+// Scooter class
+class Scooter {
+    constructor(year, color, model) {
+      this.year = year;
+      this.color = color;
+      this.model = model;
+    }
+  }
+
+  // Driver class
+  class Driver {
+    constructor(name, age, experience) {
+      this.name = name;
+      this.age = age;
+      this.experience = experience;
+    }
+  }
+
+  // PickupLocation class
+  class PickupLocation {
+    constructor(address, city) {
+      this.address = address;
+      this.city = city;
+    }
+  }
